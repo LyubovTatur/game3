bf517c3bcbab742cf69b5f29789e2f255a6a490c
LyubovTatur <lubatatur58@gmail.com>
2021-04-04 20:36:19 +0300

    done som stuf

--- game/Assets/SkriptPlayer.cs
+++ game/Assets/SkriptPlayer.cs
@@ -8,13 +8,15 @@ public class SkriptPlayer : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveVelosity;
     //public GameObject gameObject;
-    
-
-
+    private bool isFliped = false, flipState = false; 
+    private SpriteRenderer sr;
+    Animator anim;
+    public Camera camera;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -22,15 +24,56 @@ public class SkriptPlayer : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
 
+            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Flip(mouse);
+            // transform.localScale.Set(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
 
             //transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Time.deltaTime * speed);
-            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(mouse.x,transform.position.y), speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(mouse.x, transform.position.y), speed * Time.deltaTime);
+            //anim.Play("walk");
+            anim.SetBool("IsWalking", true);
             //transform.position = Vector2.MoveTowards(transform.position,mouse, speed * Time.deltaTime);
-
+            camera.transform.position = new Vector3(transform.position.x, camera.transform.position.y,-10);
             //position.x = transform.position.x;
         }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+           // anim.Play("idle");
+        }
     }
+
+    private void Flip(Vector2 mouse)
+    {
+        if (mouse.x < transform.position.x)
+        {
+            isFliped = true;
+        }
+        else
+        {
+            isFliped = false;
+        }
+        if (isFliped)
+        {
+            if (!flipState)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+                flipState = true;
+
+            }
+        }
+        else
+        {
+            if (flipState)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+
+                flipState = false;
+
+            }
+        }
+    }
+
     void FixedUpdate()
     {
 
