b4bc8766b53af5b2aea091e7f2caf38b175d198f
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2021-04-05 14:12:38 +0300

    scripts

---
+++ game/Assets/database.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class database : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+}
+
+[System.Serializable]
+public class Item
+{
+    public int id;
+    public string name;
+    public Sprite image;
+    public GameObject model;
+    public int price;
+    public int sellPrice;
+}
