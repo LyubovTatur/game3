7d8fecd18fd73e6bd3d29683c9550a18cb767340
LyubovTatur <lubatatur58@gmail.com>
2021-04-25 18:59:06 +0300

    can sell items

--- game/Assets/skripts/inventory.cs
+++ game/Assets/skripts/inventory.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.IO;
+using UnityEngine.SceneManagement;
+using System;
 
 public class inventory : MonoBehaviour
 {
@@ -20,14 +22,88 @@ public class inventory : MonoBehaviour
     public GameObject movingObject;
     public Vector3 offset;
     public SavingValues savingValues;
+    public GameObject sellingPanel;
+    private GameObject SellingItem;
 
     public void Start()
     {
-        
         Load();
+        if (SceneManager.GetActiveScene().name == "HouseScene")
+        {
+            sellingPanel.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { YesSell(); });
+            sellingPanel.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { CansellSell(); });
+            sellingPanel.transform.GetChild(3).GetComponent<Slider>().onValueChanged.AddListener(delegate { ChangeAmount(); });
+        }
+        if (SceneManager.GetActiveScene().name == "FurnitureShopScene")
+        {
+            var furniture = GameObject.Find("items");
+            for (int i = 0; i < furniture.transform.childCount; i++)
+            {
+                //furniture.transform.GetChild(i)..AddListener(delegate { ClickOnItemInShop(); });
+
+            }
+
+        }
+    }
+
+    private void ClickOnItemInShop()
+    {
+        print("ClickOnItemInShop");
+    }
+
+    private void ChangeAmount()
+    {
+        sellingPanel.transform.GetChild(3).GetComponent<Slider>().transform.GetChild(0).GetComponent<Text>().text = sellingPanel.transform.GetChild(3).GetComponent<Slider>().value.ToString();
+        if (SellingItem != null)
+        {
+
+        sellingPanel.transform.GetChild(5).GetComponent<Text>().text = (data.items[int.Parse(SellingItem.transform.GetChild(0).GetComponent<Text>().text)].sellPrice * sellingPanel.transform.GetChild(3).GetComponent<Slider>().value).ToString();
+        }
 
     }
-    
+
+    private void CansellSell()
+    {
+        sellingPanel.transform.GetChild(0).GetComponent<Text>().text = "Вы уверенны, что хотите продать itemName за amount монет?";
+        sellingPanel.transform.GetChild(3).GetComponent<Slider>().value = 1;
+        sellingPanel.SetActive(false);
+    }
+
+    private void YesSell()
+    {
+        for (int j = 0; j < sellingPanel.transform.GetChild(3).GetComponent<Slider>().value; j++)
+        {
+
+       
+            if (SellingItem.transform.GetChild(0).GetComponentInChildren<Text>().text == "")
+            {
+                print(items.Remove(items[int.Parse(SellingItem.name)]));
+                for (int i = int.Parse(SellingItem.name); i < items.Count; i++)
+                {
+                    items[i].gameObject.name = (int.Parse(items[i].gameObject.name) - 1).ToString();
+                }
+                //RemoveFromInv(currID);
+                Destroy(SellingItem);
+            }
+            else
+            {
+                items[int.Parse(SellingItem.name)].count--;
+
+                if (SellingItem.transform.GetChild(0).GetComponentInChildren<Text>().text == "2")
+                {
+                    SellingItem.transform.GetChild(0).GetComponentInChildren<Text>().text = "";
+                }
+                else
+                {
+                    SellingItem.transform.GetChild(0).GetComponentInChildren<Text>().text = (int.Parse(SellingItem.transform.GetChild(0).GetComponentInChildren<Text>().text) - 1).ToString();
+                }
+            }
+        }
+        GameObject.Find("coinAmount").GetComponent<Text>().text = (int.Parse(GameObject.Find("coinAmount").GetComponent<Text>().text) + int.Parse(sellingPanel.transform.GetChild(5).GetComponent<Text>().text)).ToString();
+        Save();
+        CansellSell();
+    }
+
     public void Update()
     {
         if (currID!=-1)
@@ -104,11 +180,13 @@ public class inventory : MonoBehaviour
             newItem.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
 
             Button itemButton = newItem.GetComponent<Button>();
+            newItem.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { SellItemPanel(); });
 
             itemButton.onClick.AddListener(delegate
             {
                 SelectObject();
             });
+           
             items.Add(ii);
         }
     }
@@ -198,6 +276,9 @@ public class inventory : MonoBehaviour
             {
                 SelectObject();
             });
+            newItem.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { SellItemPanel(); });
+
+            //newItem.GetComponentInChildren<Button>().onClick.AddListener(delegate { SellItemPanel(); });
             items.Add(ii);
             AddItem(items.Count-1, data.items[i],1);
 
@@ -293,6 +374,26 @@ public class inventory : MonoBehaviour
         //newGameObj.name = currItem.id.ToString();
         currID = -1;
         movingObject.gameObject.SetActive(false);
+    }
+
+    private void SellItemPanel()
+    {
+        SellingItem = es.currentSelectedGameObject.transform.parent.gameObject;
+        if (SellingItem.transform.GetChild(0).GetComponent<Text>().text!="")
+        {
+
+        sellingPanel.transform.GetChild(3).GetComponent<Slider>().maxValue = int.Parse(SellingItem.transform.GetChild(0).GetComponent<Text>().text);
+        }
+        else
+        {
+            sellingPanel.transform.GetChild(3).GetComponent<Slider>().maxValue = 1;
+        }
+
+        sellingPanel.transform.GetChild(0).GetComponent<Text>().text = sellingPanel.transform.GetChild(0).GetComponent<Text>().text.Replace("itemName", data.items[items[int.Parse(es.currentSelectedGameObject.transform.parent.name)].id].name);// !!!!!!!!!!!!!!!!!!!!!!!!_!_!_!_!__!_!_!__!__!__!!!!
+        sellingPanel.transform.GetChild(0).GetComponent<Text>().text = sellingPanel.transform.GetChild(0).GetComponent<Text>().text.Replace("amount", data.items[items[int.Parse(es.currentSelectedGameObject.transform.parent.name)].id].sellPrice.ToString());// !!!!!!!!!!!!!!!!!!!!!!!!_!_!_!_!__!_!_!__!__!__!!!!
+        sellingPanel.transform.GetChild(5).GetComponent<Text>().text = data.items[items[int.Parse(es.currentSelectedGameObject.transform.parent.name)].id].sellPrice.ToString();
+        sellingPanel.SetActive(true);
+
     }
 
     private void SelectItem()
@@ -391,6 +492,8 @@ public class inventory : MonoBehaviour
         {
             newList.Add(new ItemIdCount(items[i].id,items[i].count));
         }
+        savingValues.coins = int.Parse(GameObject.Find("coinAmount").GetComponent<Text>().text);
+        savingValues.diamonds = int.Parse(GameObject.Find("diamondAmount").GetComponent<Text>().text);
         savingValues.items = newList;
         File.WriteAllText(Application.dataPath + "/save.gamesave", JsonUtility.ToJson(savingValues));
         print("saved");
@@ -409,55 +512,59 @@ public class inventory : MonoBehaviour
             savingValues = new SavingValues(0, 0, new List<FixedObjects>(), new List<ItemIdCount>());
             Save();
         }
-
         GameObject.Find("coinAmount").GetComponent<Text>().text = savingValues.coins.ToString();
         GameObject.Find("diamondAmount").GetComponent<Text>().text = savingValues.diamonds.ToString();
-        foreach (var obj in savingValues.fixedObjects)
+        if (SceneManager.GetActiveScene().name == "HouseScene")
         {
-            var GameObj = (GameObject)Instantiate(data.items[obj.id].model.gameObject.gameObject, new Vector3(obj.x,obj.y,obj.z), Quaternion.identity);
-            GameObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            GameObj.name = obj.id.ToString();
-           // GameObj.GetComponent<Button>().onClick.AddListener(delegate { SelectItem(); });
-        }
-        int indForInv=0;
-        foreach (var inv in savingValues.items)
-        {
-            //if (items.Count == 0)
-            //{
-            //    AddGraphics();
-            //}
-            // х*ета рандомная :3
 
-            GameObject newItem = Instantiate(gameObjectShow, InventoryMainObject.transform) as GameObject;
-            newItem.name = indForInv.ToString();
-            ItemInventory ii = new ItemInventory();
-            ii.gameObject = newItem;
-            RectTransform rt = newItem.GetComponent<RectTransform>();
-            rt.localPosition = new Vector3(0, 0, 0);
-            rt.localScale = new Vector3(1, 1, 1);
-            newItem.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
 
-            Button itemButton = newItem.GetComponent<Button>();
-
-            itemButton.onClick.AddListener(delegate
+            foreach (var obj in savingValues.fixedObjects)
             {
-                SelectObject();
-            });
-            items.Add(ii);
+                var GameObj = (GameObject)Instantiate(data.items[obj.id].model.gameObject.gameObject, new Vector3(obj.x, obj.y, obj.z), Quaternion.identity);
+                GameObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                GameObj.name = obj.id.ToString();
+                // GameObj.GetComponent<Button>().onClick.AddListener(delegate { SelectItem(); });
+            }
+            int indForInv = 0;
+            foreach (var inv in savingValues.items)
+            {
+                //if (items.Count == 0)
+                //{
+                //    AddGraphics();
+                //}
+                // х*ета рандомная :3
+
+                GameObject newItem = Instantiate(gameObjectShow, InventoryMainObject.transform) as GameObject;
+                newItem.name = indForInv.ToString();
+                ItemInventory ii = new ItemInventory();
+                ii.gameObject = newItem;
+                RectTransform rt = newItem.GetComponent<RectTransform>();
+                rt.localPosition = new Vector3(0, 0, 0);
+                rt.localScale = new Vector3(1, 1, 1);
+                newItem.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+
+                Button itemButton = newItem.GetComponent<Button>();
+
+                itemButton.onClick.AddListener(delegate
+                {
+                    SelectObject();
+                });
+                newItem.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { SellItemPanel(); });
+                items.Add(ii);
 
 
-            //for (int i = 0; i < itemCount; i++)
-            //{
-            //    AddItem(i, data.items[i], Random.Range(1, 5));
-            //}
-            //UpdateInventory();
-            
-            AddItem(indForInv, data.items[inv.id],inv.count);
-            indForInv++;
+                //for (int i = 0; i < itemCount; i++)
+                //{
+                //    AddItem(i, data.items[i], Random.Range(1, 5));
+                //}
+                //UpdateInventory();
+
+                AddItem(indForInv, data.items[inv.id], inv.count);
+                indForInv++;
+            }
+            UpdateInventory();
         }
-        UpdateInventory();
     }
-    
 }
 [System.Serializable]
 
